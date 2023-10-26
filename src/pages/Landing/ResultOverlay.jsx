c29815c55b816ceab65b6b4c2bad4ffb05b19699
Egba Nnaemeka <egba.nnaemeka@nvit.tech>
2023-10-26 21:56:59 +0100

    Fix scroll

--- src/pages/Landing/ResultOverlay.jsx
+++ src/pages/Landing/ResultOverlay.jsx
@@ -5,6 +5,8 @@ const ResultOverlay = ({ res, setIsOpen, isOpen }) => {
   useEffect(() => {
     if (isOpen) {
       document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
     }
     return () => {
       document.body.style.overflow = 'unset';
@@ -15,7 +17,7 @@ const ResultOverlay = ({ res, setIsOpen, isOpen }) => {
         <div className="absolute flex items-center justify-center h-screen z-40 w-full bg-white/[.6] top-0 left-0">
             <div className="w-full h-full absolute" onClick={() => setIsOpen(false)}></div>
             <div className="w-[90%] bg-white px-10 z-50 rounded-xl border border-gray-500 py-8 lg:w-[40%]">
-                <p className="max-w-[90%]"><span className="font-bold text-[18px] mr-4 mb-4 text-blue">Link:</span>{res.url}</p>
+                <p className="max-w-[90%] block"><span className="font-bold text-[18px] mr-4 mb-4 text-blue">Link:</span>{res.url}</p>
                 <div><span className="font-bold text-[18px] mr-4 text-blue">AI Prediction:</span>{res.security_checks.ai_flagged === false ? <span className="text-green-500 font-bold text-[18px]">Secure</span> : <span className="text-red-500 text-[18px] font-bold">Warning: Insecure</span>}</div>
                 {/* <p><span className="font-bold text-[18px] mr-4 text-blue">URL:</span>{res.security_checks.url_flagged === false ? <span className="text-green-500 font-bold text-[18px]">Secure</span> : <p className="text-red-500 font-bold text-[18px]">Warning: Insecure</p>}</p> */}
                 <p><span className="font-semibold capitalize text-2xl text-dark">Risk Score:  </span>{res.risk_score}</p>
